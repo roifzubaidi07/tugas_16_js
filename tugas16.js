beabdf812576cdfc45372e7b9049e6698dc738aa
M. Ro'if <roifzubaidi5@gmail.com>
2021-02-07 12:37:32 +0700

    Pengumpulan tugas 16 NIOMIC

---
+++ tugas16.js
@@ -0,0 +1,8 @@
+var min1 = Math.min(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+var max1 = Math.max(45, 100, 99, 3, 2, 4, 1, 2, 3, 19, 22, 14, 10);
+var min2 = Math.min(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+var max2 = Math.max(45, 100, 99, 3, 2, 81, 22, 55, 1, 2, 3, 4, 5);
+console.log('Nilai Minimal Deret Pertama : ', min1);
+console.log('Nilai Maksimal Deret Pertama : ', max1);
+console.log('Nilai Minimal Deret Kedua : ', min2);
+console.log('Nilai Maksimal Deret Kedua : ', max2);
